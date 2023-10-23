018bd027e73865a5b464bc909bd3567db28375be
mostafizz-fuad <mostafizzfuad@gmail.com>
2023-10-24 02:05:53 +0600

    update code

---
+++ 00_courseOutline.js
@@ -0,0 +1,14 @@
+/******* prerequisites *******
+ * HTML
+ * CSS
+ * Git & Github
+ *****************************/
+
+
+/******* how to follow this course *******
+ * don't skip any video
+ * show video serially
+ * follow video description
+ * quizes in the video description
+ * be consistent
+ ****************************************/
